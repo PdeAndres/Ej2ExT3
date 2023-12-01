125ed15c7c50a86c662bcad23b248916813e0b97
PdeAndres <pablodeandresolano@gmail.com>
2023-12-01 20:10:56 +0100

    Cambios en la clase turron añadiendo que mi favorito es el de chocolate

--- ProyectoExEj2/src/Turron.java
+++ ProyectoExEj2/src/Turron.java
@@ -3,6 +3,7 @@ public class Turron {
 
 	public static void main(String[] args) {
 		System.out.println("Soy Pablo de Andrés y me encantan los turrones");
+		System.out.println("Pero mi preferido es el de chocolate");
 
 	}
 
